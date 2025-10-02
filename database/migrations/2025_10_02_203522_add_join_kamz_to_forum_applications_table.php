53221f1ba9ee147a3a0c772c9ce125064e22f1b4
amirlanqz <amirlanqz@gmail.com>
2025-10-03 01:37:26 +0500

    refactoring form v.3.2

---
+++ database/migrations/2025_10_02_203522_add_join_kamz_to_forum_applications_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('forum_applications', function (Blueprint $table) {
+            $table->string('join_kamz')->nullable()->after('is_member');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('forum_applications', function (Blueprint $table) {
+             $table->dropColumn('join_kamz');
+        });
+    }
+};
